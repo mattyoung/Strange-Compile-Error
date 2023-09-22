a576896b0ff60252285a971fc468f4f4bfee10f1
Matthew Young <luckymatty@protonmail.com>
2023-09-22 07:34:30 -0700

    Initial Commit

---
+++ CompileErrorNotunderstand/CompileErrorNotunderstandApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompileErrorNotunderstandApp.swift
+//  CompileErrorNotunderstand
+//
+//  Created by Matthew Young on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct CompileErrorNotunderstandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
